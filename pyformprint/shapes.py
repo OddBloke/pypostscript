b28a2e0d16bd9f4d8ecc547d52b0c4f75208e70a
Mark Henwood <mark@mcbh.co.uk>
2012-01-30 21:37:23 +0000

    Refactor argument validation in shapes.py
    Common validation of +ve integer argument validation now superclassed.

--- pyformprint/shapes.py
+++ pyformprint/shapes.py
@@ -1,18 +1,31 @@
-class Circle(object):
+class AllIntegerArgumentShape(object):
+    """
+    A class which takes only integer arguments during init.
+
+    """
+    def __init__(self, *args):
+        for arg in args:
+            if not (isinstance(arg, int) and arg >= 0):
+                raise ValueError(arg)
+
+
+class Circle(AllIntegerArgumentShape):
     """
     Generic representation of a circle.
 
     """
     def __init__(self, x_pts, y_pts, radius_pts, line_width_pts):
-        for arg in x_pts, y_pts, radius_pts, line_width_pts:
-            if not (isinstance(arg, int) and arg >= 0):
-                raise ValueError(arg)
+        super(Circle, self).__init__(x_pts, y_pts, radius_pts, line_width_pts)
 
         self.x_pts, self.y_pts, self.radius_pts, self.line_width_pts = \
             x_pts, y_pts, radius_pts, line_width_pts
 
     @property
     def ps(self):
+        """
+        PostScript string representation of this object.
+
+        """
         return ('newpath {x_pts} {y_pts} {radius_pts} 0 360 arc\n'
                 'closepath\n'
                 '{line_width_pts} setlinewidth\n'
@@ -22,15 +35,14 @@ class Circle(object):
                                   line_width_pts=self.line_width_pts))
 
 
-class Rectangle(object):
+class Rectangle(AllIntegerArgumentShape):
     """
     Generic representation of a rectangle.
 
     """
     def __init__(self, x_pts, y_pts, width_pts, height_pts, line_width_pts):
-        for arg in x_pts, y_pts, width_pts, height_pts, line_width_pts:
-            if not (isinstance(arg, int) and arg >= 0):
-                raise ValueError(arg)
+        super(Rectangle, self).__init__(x_pts, y_pts, width_pts, height_pts,
+                                        line_width_pts)
 
         self.x_pts, self.y_pts, self.width_pts, \
         self.height_pts, self.line_width_pts = \
@@ -38,6 +50,10 @@ class Rectangle(object):
 
     @property
     def ps(self):
+        """
+        PostScript string representation of this object.
+
+        """
         return ('newpath {x_pts} {y_pts} moveto\n'
                 '{width_pts_pos} 0 rlineto\n'
                 '0 {height_pts_pos} rlineto\n'
