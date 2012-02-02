c1058af27ff9338bc97f452b27f26152a62e3ab3
Mark Henwood <mark@mcbh.co.uk>
2012-02-02 18:26:45 +0000

    Refactor of Font classes

--- pyformprint/text.py
+++ pyformprint/text.py
@@ -1,4 +1,5 @@
-class TimesPlainFont(object):
+class Font(object):
+
     def __init__(self, size_pts):
         """
         Generic representation of a Times Roman font.
@@ -15,28 +16,15 @@ class TimesPlainFont(object):
         PostScript string representation of this object.
 
         """
-        return ('/Times-Roman findfont\n'
+        return ('/{ps_font_name} findfont\n'
                 '{size_pts} scalefont\n'
-                'setfont\n'.format(size_pts=self.size_pts))
+                'setfont\n'.format(ps_font_name=self.PS_FONT_NAME,
+                                   size_pts=self.size_pts))
 
 
-class TimesBoldFont(object):
-    def __init__(self, size_pts):
-        """
-        Generic representation of a Times Bold font.
+class TimesPlainFont(Font):
+    PS_FONT_NAME = 'Times-Roman'
 
-        """
-        if not (isinstance(size_pts, int) and size_pts > 0):
-            raise ValueError(size_pts)
 
-        self.size_pts = size_pts
-
-    @property
-    def ps(self):
-        """
-        PostScript string representation of this object.
-
-        """
-        return ('/Times-Bold findfont\n'
-                '{size_pts} scalefont\n'
-                'setfont\n'.format(size_pts=self.size_pts))
+class TimesBoldFont(Font):
+    PS_FONT_NAME = 'Times-Bold'
