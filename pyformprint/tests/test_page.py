d1942d86bd9ca11ba34d6dff299a415bed8620d5
Mark Henwood <mark@mcbh.co.uk>
2012-02-16 11:07:33 +0000

    Extend existing Page tests to include TextLine items as well

--- pyformprint/tests/test_page.py
+++ pyformprint/tests/test_page.py
@@ -2,13 +2,14 @@ from unittest2 import TestCase
 
 from pyformprint.page import Page
 from pyformprint.shapes import Circle, Rectangle
+from pyformprint.text import TextLine, TimesBoldFont
 
 
 class PageTestCase(TestCase):
 
-    def test_page_one_shape(self):
+    def test_page_one_object(self):
         """
-        Shape appended to Page should render its ps correctly.
+        Object appended to Page should render its ps correctly.
 
         """
         circle = Circle(x_pts=50,
@@ -21,9 +22,9 @@ class PageTestCase(TestCase):
         self.assertEqual(page.ps,
                          circle.ps)
 
-    def test_page_many_shapes(self):
+    def test_page_many_objects(self):
         """
-        Shapes appended to Page should have their ps rendered correctly.
+        Objects appended to Page should have their ps rendered correctly.
 
         """
         circle_1 = Circle(x_pts=50,
@@ -44,28 +45,27 @@ class PageTestCase(TestCase):
                                 height_pts=5,
                                 width_pts=37,
                                 line_width_pts=1)
+        text_line = TextLine(x_pts=250,
+                             y_pts=210,
+                             font=TimesBoldFont(size_pts=18),
+                             text='Some Text')
         page = Page()
-        page.extend(circle_1, rectangle_1, circle_2, rectangle_2)
+        page.extend(circle_1, rectangle_1, circle_2, rectangle_2, text_line)
         self.assertEqual(page.ps,
                          '\n'.join([shape.ps for shape in (circle_1,
                                                            rectangle_1,
                                                            circle_2,
-                                                           rectangle_2)]))
+                                                           rectangle_2,
+                                                           text_line)]))
 
 # Tests needed
 """
-    "page" which uses the .ps property of all components if required
+    portrait / landscape layout
 
-    "page" to include portrait/landscape setups
+    text box
 
     "@uselib('barcodes')" should put the barcode lib into the top of the output
     document
 
-    group all equivalent font/size combinations in the same block for document
 
-    TextBox object
-
-    Line wrapping awareness (Paragraph object)
-
-    Handle breaks in text - paragraph set or TextBox with Paragraphs in it
 """
