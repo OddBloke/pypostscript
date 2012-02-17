9e6cb5805c19709ab9a25c39a4808a9a37587028
Mark Henwood <mark@mcbh.co.uk>
2012-02-17 16:38:30 +0000

    ITF14Barcode class

--- pyformprint/barcode.py
+++ pyformprint/barcode.py
@@ -1,13 +1,13 @@
 from pyformprint import AllIntegerArgumentClass
 
 
-class Code39Barcode(AllIntegerArgumentClass):
+class Barcode(AllIntegerArgumentClass):
 
     required_parts = ['barcode']
 
     def __init__(self, x_pts, y_pts, chars):
 
-        super(Code39Barcode, self).__init__(x_pts, y_pts)
+        super(Barcode, self).__init__(x_pts, y_pts)
         self._x_pts, self._y_pts, self._chars = \
             x_pts, y_pts, chars
 
@@ -15,7 +15,19 @@ class Code39Barcode(AllIntegerArgumentClass):
     def ps(self):
         return ('{x_pts} {y_pts} moveto ({chars}) '
                 '(includecheck includetext)\n'
-                '/code39 /uk.co.terryburton.bwipp findresource exec\n'.format(
+                '/{barcode_code} /uk.co.terryburton.bwipp '
+                'findresource exec\n'.format(
                     x_pts=self._x_pts,
                     y_pts=self._y_pts,
-                    chars=self._chars))
+                    chars=self._chars,
+                    barcode_code=self.BARCODE_CODE))
+
+
+class Code39Barcode(Barcode):
+
+    BARCODE_CODE = 'code39'
+
+
+class ITF14Barcode(Barcode):
+
+    BARCODE_CODE = 'itf14'
--- pyformprint/tests/test_barcode.py
+++ pyformprint/tests/test_barcode.py
@@ -1,6 +1,6 @@
 from unittest2 import TestCase
 
-from pyformprint.barcode import Code39Barcode
+from pyformprint.barcode import Code39Barcode, ITF14Barcode
 
 
 class Code39BarcodeTestCase(TestCase):
@@ -19,6 +19,22 @@ class Code39BarcodeTestCase(TestCase):
             '30 500 moveto (Code 39 test) (includecheck includetext)\n'
             '/code39 /uk.co.terryburton.bwipp findresource exec\n')
 
+
+class ITF14BarcodeTestCase(TestCase):
+
+    def test_barcode(self):
+        """
+        ITF14Barcode should have 'barcode' in required_parts and render ps.
+
+        """
+        barcode = ITF14Barcode(x_pts=300,
+                               y_pts=50,
+                               chars='ITF 14 TEST')
+        self.assertIn('barcode', barcode.required_parts)
+        self.assertEqual(
+            barcode.ps,
+            '300 50 moveto (ITF 14 TEST) (includecheck includetext)\n'
+            '/itf14 /uk.co.terryburton.bwipp findresource exec\n')
 
 # Tests needed
 """
